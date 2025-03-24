57cb3653166f217c66764638ac2cd66eeed54116
Camden DeVoogd <camdendevoogd@gmail.com>
2025-03-23 19:43:06 -0600

    wip

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -17,16 +17,26 @@ https://neovim.io/doc/user/lua.html#vim.keymap.set()
 --]]
 
 local function map(mode, key, cmd, opts, defaults)
-   local merged_opts = vim.tbl_deep_extend("force", { silent = true }, defaults or {}, opts or {})
-   -- vim.keymap.set has noremap enabled by default
-   vim.keymap.set(mode, key, cmd, merged_opts)
+    local merged_opts = vim.tbl_deep_extend("force", { silent = true }, defaults or {}, opts or {})
+    -- vim.keymap.set has noremap enabled by default
+    vim.keymap.set(mode, key, cmd, merged_opts)
 end
 
-local function nmap(key, cmd, opts) return map("n", key, cmd, opts) end
-local function imap(key, cmd, opts) return map("i", key, cmd, opts) end
-local function vmap(key, cmd, opts) return map("v", key, cmd, opts) end
-local function xmap(key, cmd, opts) return map("x", key, cmd, opts) end
-local function omap(key, cmd, opts) return map("o", key, cmd, opts) end
+local function nmap(key, cmd, opts)
+    return map("n", key, cmd, opts)
+end
+local function imap(key, cmd, opts)
+    return map("i", key, cmd, opts)
+end
+local function vmap(key, cmd, opts)
+    return map("v", key, cmd, opts)
+end
+local function xmap(key, cmd, opts)
+    return map("x", key, cmd, opts)
+end
+local function omap(key, cmd, opts)
+    return map("o", key, cmd, opts)
+end
 
 -- Faster buffer navigation
 nmap("<C-h>", "<C-w>h")
@@ -64,40 +74,84 @@ nmap("<leader>Y", '"+yy', { desc = "copy line to clipboard" })
 xmap(">", ">gv")
 xmap("<", "<gv")
 
-nmap("<leader>,", function() require("core.commands").command_list_picker() end, { desc = "command list" })
+nmap("<leader>,", function()
+    require("core.commands").command_list_picker()
+end, { desc = "command list" })
 
 -- Find/Files
 nmap("<leader>fe", "<cmd>Oil<cr>", { desc = "file explorer (oil)" })
-nmap("<leader>ff", function() require("core.plugins.snacks").project_files() end, { desc = "project files" })
-nmap("<leader>fF", function() Snacks.picker.files() end, { desc = "all files" })
-nmap("<leader>fg", function() Snacks.picker.git_files() end, { desc = "git files" })
-nmap("<leader>fb", function() Snacks.picker.buffers() end, { desc = "buffers" })
-nmap("<leader>fr", function() Snacks.picker.recent({ filter = { cwd = true } }) end, { desc = "recent" })
+nmap("<leader>ff", function()
+    require("core.plugins.snacks").project_files()
+end, { desc = "project files" })
+nmap("<leader>fF", function()
+    Snacks.picker.files()
+end, { desc = "all files" })
+nmap("<leader>fg", function()
+    Snacks.picker.git_files()
+end, { desc = "git files" })
+nmap("<leader>fb", function()
+    Snacks.picker.buffers()
+end, { desc = "buffers" })
+nmap("<leader>fr", function()
+    Snacks.picker.recent({ filter = { cwd = true } })
+end, { desc = "recent" })
 
 -- Buffer
-nmap("<leader>bd", function() require("mini.bufremove").delete(0, false) end, { desc = "delete current buffer" })
+nmap("<leader>bd", function()
+    require("mini.bufremove").delete(0, false)
+end, { desc = "delete current buffer" })
 
 -- g: git
-nmap("<leader>gd", function() Snacks.picker.git_diff() end, { desc = "git diff" })
-nmap("<leader>gl", function() Snacks.picker.git_log() end, { desc = "git log" })
-nmap("<leader>gs", function() Snacks.picker.git_status() end, { desc = "git status" })
-nmap("<leader>gbl", function() Snacks.git.blame_line() end, { desc = "blame line" })
-nmap("<leader>gro", function() Snacks.gitbrowse.open() end, { desc = "open remote" })
+nmap("<leader>gd", function()
+    Snacks.picker.git_diff()
+end, { desc = "git diff" })
+nmap("<leader>gl", function()
+    Snacks.picker.git_log()
+end, { desc = "git log" })
+nmap("<leader>gs", function()
+    Snacks.picker.git_status()
+end, { desc = "git status" })
+nmap("<leader>gbl", function()
+    Snacks.git.blame_line()
+end, { desc = "blame line" })
+nmap("<leader>gro", function()
+    Snacks.gitbrowse.open()
+end, { desc = "open remote" })
 
 -- s: search
-nmap("<leader>sb", function() Snacks.picker.lines() end, { desc = "buffer" })
-nmap("<leader>sg", function() Snacks.picker.grep() end, { desc = "grep" })
-nmap("<leader>sh", function() Snacks.picker.help() end, { desc = "help" })
-nmap("<leader>su", function() Snacks.picker.undo() end, { desc = "undo" })
-nmap("<leader>ss", function() Snacks.picker.lsp_symbols() end, { desc = "symbols" })
-nmap("<leader>sS", function() Snacks.picker.lsp_workspace_symbols() end, { desc = "symbols (workspace)" })
+nmap("<leader>sb", function()
+    Snacks.picker.lines()
+end, { desc = "buffer" })
+nmap("<leader>sg", function()
+    Snacks.picker.grep()
+end, { desc = "grep" })
+nmap("<leader>sh", function()
+    Snacks.picker.help()
+end, { desc = "help" })
+nmap("<leader>su", function()
+    Snacks.picker.undo()
+end, { desc = "undo" })
+nmap("<leader>ss", function()
+    Snacks.picker.lsp_symbols()
+end, { desc = "symbols" })
+nmap("<leader>sS", function()
+    Snacks.picker.lsp_workspace_symbols()
+end, { desc = "symbols (workspace)" })
 
-nmap("<leader>pp", function() Snacks.picker.resume() end, { desc = "re-open previous picker" })
+nmap("<leader>pp", function()
+    Snacks.picker.resume()
+end, { desc = "re-open previous picker" })
 
 -- x: diagnostics & errors
-nmap("<leader>xf", function() vim.diagnostic.open_float() end, { desc = "diagnostics float" })
-nmap("<leader>xs", function() Snacks.picker.diagnostics() end, { desc = "search diagnostics" })
-nmap("<leader>xS", function() Snacks.picker.diagnostics_buffer() end, { desc = "search diagnostics (buffer)" })
+nmap("<leader>xf", function()
+    vim.diagnostic.open_float()
+end, { desc = "diagnostics float" })
+nmap("<leader>xs", function()
+    Snacks.picker.diagnostics()
+end, { desc = "search diagnostics" })
+nmap("<leader>xS", function()
+    Snacks.picker.diagnostics_buffer()
+end, { desc = "search diagnostics (buffer)" })
 nmap("<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", { desc = "trouble diagnostics" })
 nmap("<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", { desc = "trouble diagnostics (buffer)" })
 
@@ -109,11 +163,21 @@ nmap("K", vim.lsp.buf.hover)
 nmap("]d", vim.diagnostic.goto_next, { desc = " go to next diagnostics" })
 nmap("[d", vim.diagnostic.goto_prev, { desc = " go to prev diagnostic" })
 
-nmap("<leader>cgd", function() Snacks.picker.lsp_definitions() end, { desc = "go to definitions" })
-nmap("<leader>cgD", function() Snacks.picker.lsp_declarations() end, { desc = "go to declaration" })
-nmap("<leader>cgt", function() Snacks.picker.lsp_type_definitions() end, { desc = "go to type definition" })
-nmap("<leader>cgr", function() Snacks.picker.lsp_references() end, { desc = "go to references" })
-nmap("<leader>cgi", function() Snacks.picker.lsp_implementations() end, { desc = "go to implementations" })
+nmap("<leader>cgd", function()
+    Snacks.picker.lsp_definitions()
+end, { desc = "go to definitions" })
+nmap("<leader>cgD", function()
+    Snacks.picker.lsp_declarations()
+end, { desc = "go to declaration" })
+nmap("<leader>cgt", function()
+    Snacks.picker.lsp_type_definitions()
+end, { desc = "go to type definition" })
+nmap("<leader>cgr", function()
+    Snacks.picker.lsp_references()
+end, { desc = "go to references" })
+nmap("<leader>cgi", function()
+    Snacks.picker.lsp_implementations()
+end, { desc = "go to implementations" })
 
 nmap("<leader>ca", vim.lsp.buf.code_action, { desc = "code action" })
 nmap("<leader>cr", vim.lsp.buf.rename, { desc = "rename symbol" })
--- nvim/lua/core/plugins/visual.lua
+++ nvim/lua/core/plugins/visual.lua
@@ -1,28 +1,30 @@
 return {
-   {
-      "Bekaboo/dropbar.nvim",
-   },
-   {
-      "yorickpeterse/nvim-pqf",
-      opts = {},
-   },
-   {
-      "catppuccin/nvim",
-      config = function()
-         require("catppuccin").setup({
-            flavour = "mocha",
-            integrations = {
-               -- Some integrations are disabled by default, so they are being enabled here
-               mason = true,
-               mini = {
-                  enabled = true,
-                  indentscope_color = "",
-               },
-            },
-            -- Make borders between windows clearer
-            custom_highlights = function(colors) return { WinSeparator = { fg = colors.flamingo } } end,
-         })
-         vim.cmd("colorscheme catppuccin")
-      end,
-   },
+    {
+        "Bekaboo/dropbar.nvim",
+    },
+    {
+        "yorickpeterse/nvim-pqf",
+        opts = {},
+    },
+    {
+        "catppuccin/nvim",
+        config = function()
+            require("catppuccin").setup({
+                flavour = "mocha",
+                integrations = {
+                    -- Some integrations are disabled by default, so they are being enabled here
+                    mason = true,
+                    mini = {
+                        enabled = true,
+                        indentscope_color = "",
+                    },
+                },
+                -- Make borders between windows clearer
+                custom_highlights = function(colors)
+                    return { WinSeparator = { fg = colors.flamingo } }
+                end,
+            })
+            vim.cmd("colorscheme catppuccin")
+        end,
+    },
 }
