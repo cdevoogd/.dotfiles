6d2d21837559b7ff6e5ecef37a1ba0b7bfa90fe6
Camden DeVoogd <camdendevoogd@gmail.com>
2025-06-15 08:20:26 -0600

    Fix cmdline freezes on WSL

--- nvim/lua/core/plugins/blink.lua
+++ nvim/lua/core/plugins/blink.lua
@@ -8,6 +8,14 @@ return {
         sources = {
             default = { "lazydev", "lsp", "path", "snippets", "buffer" },
             providers = {
+                cmdline = {
+                    -- Stop freezes when using :! on WSL
+                    -- https://github.com/Saghen/blink.cmp/issues/795
+                    -- https://github.com/Saghen/blink.cmp/pull/1167
+                    enabled = function()
+                        return vim.fn.getcmdtype() ~= ":" or not vim.fn.getcmdline():match("^[%%0-9,'<>%-]*!")
+                    end,
+                },
                 snippets = {
                     opts = {
                         search_paths = {
